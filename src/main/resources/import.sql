6bd061e341e5094cdecacdd46f6092d218ba99a7
Kayky Oliveira <kaykycf1245@gmail.com>
2024-11-27 00:23:15 -0300

    BCrypt password

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$TA1yxzlwNf831bJLBOtcYeVa5XB5ZXwCUdDcIUCQr3qYx1RMD3Alq');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$TA1yxzlwNf831bJLBOtcYeVa5XB5ZXwCUdDcIUCQr3qYx1RMD3Alq');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
